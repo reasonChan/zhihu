7d61c71b25e484b6aa3edb02818d9dcb5e0dc451
unknown <1251326862@qq.com>
2023-07-03 17:33:43 +0800

    07-03

--- src/component/HomeHead.jsx
+++ src/component/HomeHead.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-const HomeHead = function HomeHead() {
+import React, {useMemo} from "react";
+import avatar from '../assets/images/default.png'
+import './HomeHead.less'
+const HomeHead = function HomeHead(props) {
+    let { timeStamp } = props;
+    let time = useMemo(() => {
+        const date = new Date(timeStamp)
+        const monthDict = ['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月']
+        let month = monthDict[date.getMonth()]
+        let day = date.getDay().toString().padStart(2, '0')
+        return { month, day }
+    }, [timeStamp])
     return (
         <header className="home-head-box">
-            头部
+            <div className="info">
+                <div className="time">
+                    <span>{time.day}</span>
+                    <span>{time.month}</span>
+                </div>
+                <h2 className="title">知乎日报</h2>
+            </div>
+            <div className="picture">
+                <img src={avatar} alt=""/>
+            </div>
         </header>
     );
 };
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-
+import React, { useState } from "react";
+import HomeHead from "../component/HomeHead";
 const Home = function Home() {
+    let [timeStamp, setTimeStamp] = useState(()=>Date.now())
     return (
-        <div className="detail">
-            Home
+        <div className="home-box">
+            <HomeHead timeStamp={timeStamp}/>
         </div>
     )
 }
